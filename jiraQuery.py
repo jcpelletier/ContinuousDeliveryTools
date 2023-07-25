99c4c65f929d1af3946863c67c605e7c51ac97a3
Joel Pelletier <joel.pelletier@jpelletier.com>
2023-07-25 09:02:07 -0400

    Created Sprint Report
    - pass email, api_token, domain, and a jql query and it will return the query results in json

---
+++ jiraQuery.py
@@ -0,0 +1,26 @@
+import requests
+import base64
+import json
+import sys  # Used to read command line arguments
+
+#This tool will pull 
+email = sys.argv[1]  # Use the first command line argument as the email
+api_token = sys.argv[2]  # Use the second command line argument as the API token
+domain = sys.argv[3]  # Use the third command line argument as the domain
+jql = sys.argv[4]  # Use the fourth command line argument as the JQL query
+
+headers = {
+    'Authorization': f'Basic {base64.b64encode(f"{email}:{api_token}".encode()).decode()}',
+    'Accept': 'application/json',
+}
+
+response = requests.get(
+    f'https://{domain}.atlassian.net/rest/api/3/search?jql={jql}&expand=renderedFields,changelog',
+    headers=headers
+)
+
+data = response.json()
+
+# Save the data to a file
+with open('issue_data.json', 'w') as file:
+    json.dump(data, file, indent=4)
