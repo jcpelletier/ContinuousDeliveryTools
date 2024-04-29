e8c7ce2f7c4cf40bb15358b150154ed2ae19d8b4
Joel Pelletier <joel.pelletier@invene.com>
2024-04-29 17:34:35 -0400

    Add Playwright project
    blank playwright project with basic "smoketest" that just loads and takes a screenshot of Google.com

---
+++ Playwright/smoketest.js
@@ -0,0 +1,11 @@
+const { chromium } = require('playwright');
+
+(async () => {
+    const browser = await chromium.launch({
+        headless: false // Set to false to see the browser UI
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com/');
+    await page.screenshot({ path: `example.png` });
+    await browser.close();
+})();
